1980c56698823c524c6a7b2727fa80b4799ca511
doebi <christoph@doeberl.at>
2015-03-07 04:52:57 +0100

    fixed include derp

--- quidapi/api.py
+++ quidapi/api.py
@@ -1,4 +1,4 @@
-from rest_framework import routers, serializers, viewsets
+from rest_framework import serializers, viewsets
 from data.models import Team, League
 
 
--- quidapi/urls.py
+++ quidapi/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from quidapi.views import Index, Games, Teams
 
+from rest_framework import routers
 from quidapi.api import LeagueViewSet, TeamViewSet
 
 admin.autodiscover()
