4010e5357c822f3c7713534b0443704e17496cac
doebi <christoph@doeberl.at>
2015-03-07 04:33:50 +0100

    routes for public listings /games /teams etc. + added DRF for full feature REST API

--- quidapi/urls.py
+++ quidapi/urls.py
@@ -1,11 +1,45 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from quidapi.views import Index, Games, Teams
 
-from quidapi.views import Index
+from rest_framework import routers, serializers, viewsets
+from data.models import Team, League
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', Index.as_view(), name='index'),
-    url(r'^admin/', include(admin.site.urls)),
+class LeagueSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = League
+        fields = ('name', 'code')
+
+class LeagueViewSet(viewsets.ModelViewSet):
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+
+class TeamSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Team
+        fields = ('name',)
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'leagues' , LeagueViewSet)
+router.register(r'teams'   , TeamViewSet)
+
+
+
+urlpatterns = patterns('' ,
+    url(r'^$'      , Index.as_view() , name='index') ,
+    url(r'^games/' , Games.as_view() , name='games') ,
+    url(r'^teams/' , Teams.as_view() , name='teams') ,
 )
+
+urlpatterns += [
+    url(r'^admin/'    , include(admin.site.urls))     ,
+    url(r'^api/'      , include(router.urls))         ,
+    url(r'^api-auth/' , include('rest_framework.urls' , namespace='rest_framework'))
+]
--- quidapi/views.py
+++ quidapi/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render_to_response
 from data.models import League, Team, Person, Game
 
 class Index(View):
-
     def get(self, request, *args, **kwargs):
         context = {}
         context['leagues'] = League.objects.count()
@@ -11,3 +10,17 @@ class Index(View):
         context['players'] = Person.objects.count()
         context['games'] = Game.objects.count()
         return render_to_response("index.html", context)
+
+
+class Games(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['games'] = Game.objects.all()
+        return render_to_response("games.html", context)
+
+
+class Teams(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['teams'] = Team.objects.all()
+        return render_to_response("teams.html", context)
